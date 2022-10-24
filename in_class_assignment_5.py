9ad9602a1471f045ffbb5511ff4994668be98734
Ky <59940512+KyKaywhy@users.noreply.github.com>
2022-10-23 22:44:09 -0400

    Update in_class_assignment_5.py

--- in_class_assignment_5.py
+++ in_class_assignment_5.py
@@ -1,43 +1,54 @@
-#Problem 1. Sort With Quicksort.
-# Please build a function called "quicksort" that uses recursion to define the quicksort algorithm for a list of any length. 
-# Build a main script that reads in the list provided, "numbers.txt", and runs it through your quicksort algorithm. 
-# The main script should return the finished sorted list as "sorted.txt"
-# All 3 files "In_class_assignment_5.py", "numbers.txt", and "sorted.txt" should all be added to your github repository and submitted as a github link.
+#Problem 1:
+#References (if any):
+#https://realpython.com/python-recursion/ (P.S. Thanks Brianna)
+#https://vegibit.com/how-to-read-and-write-files-in-python/
+#https://www.geeksforgeeks.org/reading-writing-text-files-python/
 
+import re
 
-'''Info on Quicksort Algorithm: 
-The Quicksort algorithm is an efficient sorting algorithm developed by British computer scientist Tony Hoare in 1959.
-
-Quicksort is a divide-and-conquer algorithm. Suppose you have a list of objects to sort. You start by choosing an item in the list, called the *pivot item*. 
-This can be any item in the list. You then partition the list into two sublists based on the pivot item and recursively sort the sublists.
-
-The steps of the algorithm are as follows:
-
-1. Choose the pivot item.
-2. Partition the list into two sublists:
-        Those items that are less than the pivot item
-        Those items that are greater than the pivot item
-3. Quicksort the sublists recursively.
-4. Each partitioning produces smaller sublists, so the algorithm is reductive. 
-
-The base cases occur when the sublists are either empty or have one element, as these are inherently sorted. 
- '''
-
-
+#GIVEN: quicksort function
 def quicksort(numbers_in_a_list):
+    if len(numbers_in_a_list) <= 1:
+        return numbers_in_a_list
+    
+    else:
+        numArray = numbers_in_a_list[0]
+        num1 = []
+        num2 = []
+        
+        for x in range(1, len(numbers_in_a_list)):
+            if numbers_in_a_list[x] < numArray:
+                num1.append(numbers_in_a_list[x])
+                
+            if numbers_in_a_list[x] > numArray:
+                num2.append(numbers_in_a_list[x])
 
-#WRITE YOUR CODE HERE FOR THE RECURSIVE SORTING FUNCTION
+    return quicksort(num1) + [numArray] + quicksort(num2)
 
-    return #WHAT DOES IT RETURN?
+#Reads in number.txt
+def read_number_txt(readNumber):
+    nums = []
+    with open(readNumberTxt) as file_object:
+        numLine = file_object.readLine()
+        fileNumbers = re.findall("[0-9]+", numLine)
+        
+        for y in fileNumbers:
+            z = int(z)
+            nums.append(z)
+    return nums
 
-
+#Writes the resulting output into sorted.txt
+def write_sorted_txt(writeSorted, fileNumbers):
+    with open(writeSorted, "w") as sorted_numbers:
+        for b in quicksort(fileNumbers):
+            b = str(b)
+            sorted_numbers.write(f"{b}\n")
+            
+#GIVEN: main function
 def main():
+    fileNumbers = read("C:\Users\Ky-long-PC\INFO450\numbers.txt") #Calls the read function for numbers.txt
+    return write("C:\Users\Ky-long-PC\INFO450\sorted.txt", fileNumbers) #Calls the write function for sorted.txt
 
-# WRITE YOUR MAIN FUNCTION HERE TO READ IN YOUR numbers.txt FILE, RUN THE LIST THROUGH YOUR SORTING ALGORITHM, 
-# AND WRITE OUT YOUR FILE
-
-    return #WHAT DOES IT RETURN?
-
-
+#GIVEN:
 if __name__ == "__main__":
     main()
